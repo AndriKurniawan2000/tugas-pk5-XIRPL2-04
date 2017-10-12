330b5c5088a5a2277ce973fe39b542d0d39a2a43
AndriKurniawan2000 <30396262+AndriKurniawan2000@users.noreply.github.com>
2017-10-12 17:13:59 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Form Register</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+<div id="form">
+	<div class="content">
+		<h1>INSTAGRAM</h1>
+		<hr width="80%">
+		<h3>Sign up to see photos and videos<br>from your fiends</h3>
+		<br>
+
+		<form action="input.php" method="GET">
+		<input type="text" name="email" placeholder="Mobile Number or Email"><br>
+		<input type="text" name="fullname" placeholder="Full Name"><br>
+		<input type="text" name="username" placeholder="Uername"><br>
+		<input type="password" name="password" placeholder="Password"><br>
+		
+
+		<button type="submit" value="Sign Up">Sign Up</button><br>
+</form>
+		
+		<p style="margin: 5px; color: black">OR</p>
+		<button>Login with Facebook</button>
+		<p>By signing up, you agree to our<br><b>Terms & Privacy Policy</b>.</p>
+	</div>
+</center>
+</div>
+<center>
+
+</body>
+</html>
